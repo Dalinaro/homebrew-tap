b28fa0353b6cf0f126c78431e0b47ff3123b65dc
Dalinaro <39046193+Dalinaro@users.noreply.github.com>
2026-07-08 10:01:37 +0300

    ttvpn 1.21

--- Casks/ttvpn.rb
+++ Casks/ttvpn.rb
@@ -1,6 +1,6 @@
 cask "ttvpn" do
-  version "1.20"
-  sha256 "2283ce1bcc611e80dd326865996456dba6b837270b22ffad9d54745f7fbca508"
+  version "1.21"
+  sha256 "a954d17cf0de385d3f77afc75c3f995d43f26f885a76fd36e3fd3436b33c96cb"
 
   url "https://github.com/Dalinaro/ttvpn/releases/download/v#{version}/TT-Vpn.dmg"
   name "TT Vpn"
